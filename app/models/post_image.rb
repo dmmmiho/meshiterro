3a258ecefb248399a0708508931b897927e90970
miho abe <106742176+dmmmiho@users.noreply.github.com>
2022-07-16 11:51:24 +0000

    initial commit

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -4,12 +4,11 @@ has_one_attached :image
 belongs_to :user
 
  def get_image
-  if image.attached?
+  unless image.attached?
     file_path = Rails.root.join('app/assets/images/no_image.jpg')
-    image.attach(io: File.open(file_path),filename: 'default-image.jpg', content_type: 'image')
+    image.attach(io: File.open(file_path),filename: 'default-image.jpg', content_type: 'image/jpeg')
   end 
     image
-
  end 
-end 
  
+end 
